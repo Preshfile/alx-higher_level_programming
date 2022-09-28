0bb0a072b260dd32a9e507d28c888a138e65c4a7
Preshfile <preshcharlies2014.com>
2022-09-28 15:37:13 -0700

    CPython

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,2 +1,5 @@
-def square_matrix_simple(matrix=[]):
-	return [(val**2 for val in row) for now in column]
+def square_matrix_simple(matrix=[]i):
+	new_list = matrix.copy()
+	for i in range(len(matrix)):
+		new_list[i] = list(map(lambda x: x**2, matrix[i]))
+		return new_list
