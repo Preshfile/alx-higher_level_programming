b5487b6f5f5c70fccb0dd7e9a95ee9f27130dc74
Preshfile <preshcharlies2014.com>
2022-09-28 15:42:30 -0700

    CPython #1: PyBytesObject

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,4 +1,4 @@
-def square_matrix_simple(matrix=[]i):
+def square_matrix_simple(matrix=[]):
 	new_list = matrix.copy()
 	for i in range(len(matrix)):
 		new_list[i] = list(map(lambda x: x**2, matrix[i]))
