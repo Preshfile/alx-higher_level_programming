bc905e0600855dbbd71418c6a7bb979d4a0af935
Preshfile <preshcharlies2014@gmail.com>
2023-01-17 15:34:49 -0800

    Error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,10 +8,18 @@ with the letter as a parameter.
 
 import sys
 import requests
-if name == "main":
-url = sys.argv[1]
-response = requests.get(url)
-if response.status_code >= 400:
-print("Error code: {}".format(response.status_code))
-else:
-print(response.text)
+
+def check_response(url):
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1]
+    except IndexError:
+        print("Usage: check_response.py <URL> - handles HTTP errors")
+        sys.exit(1)
+    check_response(url)
