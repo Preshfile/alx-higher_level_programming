fd591e34cce2641f638693ec1e8cb32e8e136798
Preshfile <preshcharlies2014@gmail.com>
2023-01-17 14:28:30 -0800

    POST an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""This is  a Python script that takes in a URL and an email address, 
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+
+
+
+import sys
+import requests
+
+if name == "main":
+url = sys.argv[1]
+email = sys.argv[2]
+data = {'email': email}
+
+Copy code
+r = requests.post(url, data=data)
+print(r.text)
