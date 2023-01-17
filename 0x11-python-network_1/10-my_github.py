0a9396a303d8a32177c1164f9dad6b0b3eafa6ca
Preshfile <preshcharlies2014@gmail.com>
2023-01-17 15:47:28 -0800

    My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,23 +4,18 @@
 (username and password) and uses the GitHub API to display your id
 """
 
-#!/usr/bin/python3
 import requests
 import sys
 
-# Get username and personal access token from command line arguments
 username = sys.argv[1]
 token = sys.argv[2]
 
-# Set up basic authentication with the personal access token
 auth = requests.auth.HTTPBasicAuth(username, token)
 
-# Make a GET request to the GitHub API to retrieve the user's information
 response = requests.get("https://api.github.com/user", auth=auth)
 
-# Check if the request was successful
 if response.status_code == 200:
-    # Extract the user's id from the response
+    
     user_id = response.json().get("id")
     print(user_id)
 else:
