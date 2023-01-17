ba37ccf605f4beea54c8a5fb69f20bba00fbac5c
Preshfile <preshcharlies2014@gmail.com>
2023-01-17 14:33:03 -0800

    POST an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,15 +5,13 @@ sends a POST request to the passed URL with the email as a parameter,
 and finally displays the body of the response.
 """
 
-
-
 import sys
 import requests
 
 if name == "main":
-url = sys.argv[1]
+endpoint = sys.argv[1]
 email = sys.argv[2]
-data = {'email': email}
+payload = {'email': email}
 
-r = requests.post(url, data=data)
-print(r.text)
+response = requests.post(endpoint, data=payload)
+print(response.text)
