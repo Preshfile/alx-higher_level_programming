839781e0d2edb42cfc1d9ebb981ad6fff732c6bf
Preshfile <preshcharlies2014@gmail.com>
2023-01-17 14:17:55 -0800

    Response header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 """a Python script that takes in a URL, sends a request to the URL 
 and displays the value of the variable X-Request-Id in the response header
@@ -8,7 +8,10 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    target_url = sys.argv[1]
-    response = requests.get(target_url)
+    url = sys.argv[1]
+    response = requests.get(url)
     request_id = response.headers.get("X-Request-Id")
-    print(request_id)
+    if request_id:
+        print(request_id)
+    else:
+        print("No 'X-Request-Id' found in the response header.")
