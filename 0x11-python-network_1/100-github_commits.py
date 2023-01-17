867f4fe1aa1579f8b6cbcbaae9557715c61336f6
Preshfile <preshcharlies2014@gmail.com>
2023-01-17 15:29:04 -0800

    Time for an interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,20 +3,21 @@
 """This is evaluates candidates applying for a back-end position
 with multiple technical challenges, like this one:
     """
-
-import requests
 import sys
+import requests
 
-repo_name = sys.argv[1]
-owner_name = sys.argv[2]
+if __name__ == "__main__":
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repository}/commits"
 
-url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-response = requests.get(url)
-
-commits = response.json()
-
-for commit in commits[:10]:
-    sha = commit["sha"]
-    author = commit["commit"]["author"]["name"]
-    print(f"{sha}: {author}")
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            sha = commits[i].get("sha")
+            author = commits[i].get("commit").get("author").get("name")
+            print(f"{sha}: {author}")
+    except IndexError:
+        pass
 
